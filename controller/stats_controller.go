3375ae91c4dbe47163e5a1225c80beb1290eb8a4
franciscomp481 <franciscopedroso@proton.me>
2024-08-18 01:35:39 +0100

    Added a swagger

--- controller/stats_controller.go
+++ controller/stats_controller.go
@@ -9,17 +9,37 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// stats_controller handles the stats usecase
 type stats_controller struct {
 	usecase usecase.StatsUsecase
 }
 
+// NewStatsController creates a new stats controller
 func NewStatsController(usecase usecase.StatsUsecase) stats_controller {
 	return stats_controller{usecase: usecase}
 }
 
+// GetPlayerStats godoc
+// @Summary Get player statistics
+// @Description Get statistics for a player based on name, index, and nationality
+// @Tags stats
+// @Accept  json
+// @Produce  json
+// @Param name query string true "Player Name"
+// @Param index query string true "Index"
+// @Param nationality query string false "Nationality"
+// @Success 200 {object} model.PlayerStats
+// @Failure 400 {object} model.Response
+// @Failure 404 {object} gin.H
+// @Failure 500 {object} gin.H
+// @Router /playerstats [get]
 func (s *stats_controller) GetPlayerStats(ctx *gin.Context) {
 
 	playerName, index, nationality := ctx.Query("name"), ctx.Query("index"), ctx.Query("nationality")
+
+	if index == "" {
+		index = "0"
+	}
 
 	index_int, err := strconv.Atoi(index)
 	if err != nil {
